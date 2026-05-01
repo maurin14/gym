48ae548b3e6cfb0c1e61e9dcc090d684291612dc
maurin14 <alison.ramirez.medina@est.una.ac.cr>
2026-05-01 16:05:10 -0600

    base project structure gym

---
+++ src/test/java/com/una/ac/cr/gym/GymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.una.ac.cr.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
