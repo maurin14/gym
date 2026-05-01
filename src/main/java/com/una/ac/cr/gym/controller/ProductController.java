93f6dac2a16c924df3aadd0b31f0a8cbafe03871
maurin14 <alison.ramirez.medina@est.una.ac.cr>
2026-05-01 16:29:39 -0600

    agregar archivos iniciales del modulo Producto

---
+++ src/main/java/com/una/ac/cr/gym/controller/ProductController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.una.ac.cr.gym.controller;
+
+/**
+ *
+ * @author alira
+ */
+public class ProductController {
+    
+}
