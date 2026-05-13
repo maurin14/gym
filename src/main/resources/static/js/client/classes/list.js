170c13e1965c516e9bfdbedd2df760093ab7d158
AmandaMonteroR <monteroramanda@gmail.com>
2026-05-13 11:31:57 -0600

    correcciones

--- src/main/resources/static/js/client/classes/list.js
+++ src/main/resources/static/js/client/classes/list.js
@@ -1,3 +1,8 @@
+let classes = [];
+let currentPage = 1;
+
+const rowsPerPage = 5;
+
 document.addEventListener("DOMContentLoaded", function () {
 
     loadClasses();
@@ -10,49 +15,100 @@ function loadClasses() {
         .then(response => response.json())
         .then(data => {
 
-            const tbody =
-                    document.getElementById("classesTableBody");
+            classes = data;
+            currentPage = 1;
 
-            tbody.innerHTML = "";
-
-            data.forEach(gymClass => {
-
-                tbody.innerHTML += `
-                    <tr>
-
-                        <td>${gymClass.classType}</td>
-
-                        <td>
-                            ${gymClass.trainer
-                                ? gymClass.trainer.fullName
-                                : "Sin entrenador"}
-                        </td>
-
-                        <td>${gymClass.classDate}</td>
-
-                        <td>${gymClass.startTime}</td>
-
-                        <td>${gymClass.endTime}</td>
-
-                        <td>${gymClass.duration} min</td>
-
-                        <td>${gymClass.maxCapacity}</td>
-
-                        <td>${gymClass.difficultyLevel}</td>
-
-                        <td>${gymClass.description}</td>
-
-                        <td>
-                            <a class="btn-primary"
-                               href="/client/attendances/form?classId=${gymClass.idClass}">
-                                Registrar
-                            </a>
-                        </td>
-
-                    </tr>
-                `;
-            });
+            showClasses();
+            showPagination();
 
         });
 
+}
+
+function showClasses() {
+
+    const tbody =
+            document.getElementById("classesTableBody");
+
+    tbody.innerHTML = "";
+
+    const start = (currentPage - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    const pageClasses = classes.slice(start, end);
+
+    pageClasses.forEach(gymClass => {
+
+        tbody.innerHTML += `
+            <tr>
+
+                <td>${gymClass.classType}</td>
+
+                <td>${gymClass.trainerName}</td>
+
+                <td>${gymClass.classDate}</td>
+
+                <td>${gymClass.startTime}</td>
+
+                <td>${gymClass.endTime}</td>
+
+                <td>${gymClass.duration} min</td>
+
+                <td>${gymClass.maxCapacity}</td>
+
+                <td>${gymClass.difficultyLevel}</td>
+
+                <td>${gymClass.description}</td>
+
+                <td>
+                    <a class="btn-primary"
+                       href="/client/attendances/form?classId=${gymClass.idClass}">
+                        Registrar
+                    </a>
+                </td>
+
+            </tr>
+        `;
+    });
+
+}
+
+function showPagination() {
+
+    const totalPages = Math.ceil(classes.length / rowsPerPage);
+
+    document.getElementById("pageInfo").innerText =
+            "Página " + currentPage + " de " + totalPages;
+
+    document.getElementById("prevBtn").style.display =
+            currentPage === 1 ? "none" : "inline-block";
+
+    document.getElementById("nextBtn").style.display =
+            currentPage === totalPages ? "none" : "inline-block";
+}
+
+function nextPage() {
+
+    const totalPages = Math.ceil(classes.length / rowsPerPage);
+
+    if (currentPage < totalPages) {
+
+        currentPage++;
+
+        showClasses();
+        showPagination();
+
+    }
+}
+
+function previousPage() {
+
+    if (currentPage > 1) {
+
+        currentPage--;
+
+        showClasses();
+        showPagination();
+
+    }
 }
--- src/main/resources/static/js/trainer/classes/list.js
+++ src/main/resources/static/js/trainer/classes/list.js
@@ -1,44 +1,136 @@
+let classes = [];
+let currentPage = 1;
+
+const rowsPerPage = 5;
+
 document.addEventListener("DOMContentLoaded", function () {
     loadClasses();
 });
 
 function loadClasses() {
+
     fetch("/classes")
         .then(response => response.json())
         .then(data => {
-            const tbody = document.getElementById("classesTableBody");
-            tbody.innerHTML = "";
 
-            data.forEach(gymClass => {
-                tbody.innerHTML += `
-                    <tr>
-                        <td>${gymClass.classType}</td>
-                        <td>${gymClass.trainer.fullName}</td>
-                        <td>${gymClass.classDate}</td>
-                        <td>${gymClass.startTime}</td>
-                        <td>${gymClass.endTime}</td>
-                        <td>${gymClass.duration} min</td>
-                        <td>${gymClass.maxCapacity}</td>
-                        <td>${gymClass.enrolledCount}</td>
-                        <td>${gymClass.difficultyLevel}</td>
-                        <td>${gymClass.description}</td>
-                        <td class="actions">
-                            <a class="btn-primary" href="/trainer/classes/form/${gymClass.idClass}">Editar</a>
-                            <button type="button" class="btn-danger" onclick="deleteClass(${gymClass.idClass})">Eliminar</button>
-                        </td>
-                    </tr>
-                `;
-            });
+            classes = data;
+            currentPage = 1;
+
+            showClasses();
+            showPagination();
+
         });
+
+}
+
+function showClasses() {
+
+    const tbody =
+            document.getElementById("classesTableBody");
+
+    tbody.innerHTML = "";
+
+    const start = (currentPage - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    const pageClasses = classes.slice(start, end);
+
+    pageClasses.forEach(gymClass => {
+
+        tbody.innerHTML += `
+            <tr>
+
+                <td>${gymClass.classType}</td>
+
+                <td>${gymClass.trainerName}</td>
+
+                <td>${gymClass.classDate}</td>
+
+                <td>${gymClass.startTime}</td>
+
+                <td>${gymClass.endTime}</td>
+
+                <td>${gymClass.duration} min</td>
+
+                <td>${gymClass.maxCapacity}</td>
+
+                <td>${gymClass.enrolledCount}</td>
+
+                <td>${gymClass.difficultyLevel}</td>
+
+                <td>${gymClass.description}</td>
+
+                <td class="actions">
+
+                    <a class="btn-primary"
+                       href="/trainer/classes/form/${gymClass.idClass}">
+                        Editar
+                    </a>
+
+                    <button type="button"
+                            class="btn-danger"
+                            onclick="deleteClass(${gymClass.idClass})">
+                        Eliminar
+                    </button>
+
+                </td>
+
+            </tr>
+        `;
+    });
+
+}
+
+function showPagination() {
+
+    const totalPages = Math.ceil(classes.length / rowsPerPage);
+
+    document.getElementById("pageInfo").innerText =
+            "Página " + currentPage + " de " + totalPages;
+
+    document.getElementById("prevBtn").style.display =
+            currentPage === 1 ? "none" : "inline-block";
+
+    document.getElementById("nextBtn").style.display =
+            currentPage === totalPages ? "none" : "inline-block";
+}
+
+function nextPage() {
+
+    const totalPages = Math.ceil(classes.length / rowsPerPage);
+
+    if (currentPage < totalPages) {
+
+        currentPage++;
+
+        showClasses();
+        showPagination();
+
+    }
+}
+
+function previousPage() {
+
+    if (currentPage > 1) {
+
+        currentPage--;
+
+        showClasses();
+        showPagination();
+
+    }
 }
 
 function deleteClass(idClass) {
-    if (confirm("¿Desea eliminar esta clase?")) {
-        fetch("/classes/" + idClass, {
-            method: "DELETE"
-        }).then(() => {
-            alert("Clase eliminada correctamente");
-            loadClasses();
-        });
-    }
+
+    fetch("/classes/" + idClass, {
+        method: "DELETE"
+    })
+    .then(() => {
+
+        alert("Clase eliminada correctamente");
+
+        loadClasses();
+
+    });
 }
