246778bf4a17a2d205cd6684d987baf515582315
AmandaMonteroR <monteroramanda@gmail.com>
2026-05-12 20:54:16 -0600

    Correcciones

--- src/main/resources/static/js/client/classes/list.js
+++ src/main/resources/static/js/client/classes/list.js
@@ -1,33 +1,58 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     loadClasses();
+
 });
 
 function loadClasses() {
+
     fetch("/classes")
         .then(response => response.json())
         .then(data => {
-            const tbody = document.getElementById("classesTableBody");
+
+            const tbody =
+                    document.getElementById("classesTableBody");
+
             tbody.innerHTML = "";
 
             data.forEach(gymClass => {
+
                 tbody.innerHTML += `
                     <tr>
+
                         <td>${gymClass.classType}</td>
-                        <td>${gymClass.trainer.fullName}</td>
-                        <td>${gymClass.classDate}</td>
-                        <td>${gymClass.startTime}</td>
-                        <td>${gymClass.endTime}</td>
-                        <td>${gymClass.duration} min</td>
-                        <td>${gymClass.maxCapacity}</td>
-                        <td>${gymClass.difficultyLevel}</td>
-                        <td>${gymClass.description}</td>
+
                         <td>
-                            <a class="btn-primary" href="/client/attendances/form?classId=${gymClass.idClass}">
+                            ${gymClass.trainer
+                                ? gymClass.trainer.fullName
+                                : "Sin entrenador"}
+                        </td>
+
+                        <td>${gymClass.classDate}</td>
+
+                        <td>${gymClass.startTime}</td>
+
+                        <td>${gymClass.endTime}</td>
+
+                        <td>${gymClass.duration} min</td>
+
+                        <td>${gymClass.maxCapacity}</td>
+
+                        <td>${gymClass.difficultyLevel}</td>
+
+                        <td>${gymClass.description}</td>
+
+                        <td>
+                            <a class="btn-primary"
+                               href="/client/attendances/form?classId=${gymClass.idClass}">
                                 Registrar
                             </a>
                         </td>
+
                     </tr>
                 `;
             });
+
         });
+
 }
