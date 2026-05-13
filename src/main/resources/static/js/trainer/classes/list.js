0e55ee334d465143e15f6dfd9d2aea218fcc76c2
AmandaMonteroR <monteroramanda@gmail.com>
2026-05-13 10:23:55 -0600

    Correccion

--- src/main/resources/static/js/trainer/classes/list.js
+++ src/main/resources/static/js/trainer/classes/list.js
@@ -1,26 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
-    loadAttendances();
+    loadClasses();
 });
 
-function loadAttendances() {
-    fetch("/attendances")
+function loadClasses() {
+
+    fetch("/trainer/classes")
         .then(response => response.json())
         .then(data => {
-            const tbody = document.getElementById("attendancesTableBody");
+
+            const tbody = document.getElementById("classesTableBody");
             tbody.innerHTML = "";
 
-            data.forEach(attendance => {
+            data.forEach(gymClass => {
+
                 tbody.innerHTML += `
                     <tr>
-                        <td>${attendance.client.fullName}</td>
-                        <td>${attendance.gymClass.classType}</td>
-                        <td>${attendance.attendanceDate}</td>
-                        <td><span class="badge status-active">${attendance.attendanceStatus}</span></td>
-                        <td>${attendance.observation}</td>
-                        <td>${attendance.registerDate}</td>
+                        <td>${gymClass.classType}</td>
+                        <td>${gymClass.description}</td>
+                        <td>${gymClass.startTime}</td>
+                        <td>${gymClass.endTime}</td>
+                        <td>${gymClass.capacity}</td>
+                        <td>${gymClass.difficulty}</td>
+                        <td>${gymClass.instructor.fullName}</td>
+                        <td>${gymClass.registerDate}</td>
+
                         <td class="actions">
-                            <a class="btn-primary" href="/trainer/attendances/form/${attendance.idAttendance}">Editar</a>
-                            <button type="button" class="btn-danger" onclick="deleteAttendance(${attendance.idAttendance})">Eliminar</button>
+
+                            <a class="btn-primary"
+                               href="/trainer/classes/form/${gymClass.idClass}">
+                               Editar
+                            </a>
+
+                            <button type="button"
+                                    class="btn-danger"
+                                    onclick="deleteClass(${gymClass.idClass})">
+                                Eliminar
+                            </button>
+
                         </td>
                     </tr>
                 `;
@@ -28,13 +44,18 @@ function loadAttendances() {
         });
 }
 
-function deleteAttendance(idAttendance) {
-    if (confirm("¿Desea eliminar esta asistencia?")) {
-        fetch("/attendances/" + idAttendance, {
+function deleteClass(idClass) {
+
+    if (confirm("¿Desea eliminar esta clase?")) {
+
+        fetch("/trainer/classes/" + idClass, {
             method: "DELETE"
-        }).then(() => {
-            alert("Asistencia eliminada correctamente");
-            loadAttendances();
+        })
+        .then(() => {
+
+            alert("Clase eliminada correctamente");
+            loadClasses();
+
         });
     }
 }
