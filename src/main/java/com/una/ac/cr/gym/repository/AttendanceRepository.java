d65910b779d6ff508731499c2d3428cd250c97b7
AmandaMonteroR <monteroramanda@gmail.com>
2026-05-28 13:15:20 -0600

    Cambios

--- src/main/java/com/una/ac/cr/gym/repository/AttendanceRepository.java
+++ src/main/java/com/una/ac/cr/gym/repository/AttendanceRepository.java
@@ -1,33 +1,77 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.una.ac.cr.gym.repository;
-
-/**
- *
- * @author Amanda
- */
-
 
 import com.una.ac.cr.gym.domain.Attendance;
 import com.una.ac.cr.gym.domain.User;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {
+
+    @Override
+    @EntityGraph(attributePaths = {
+        "client",
+        "gymClass",
+        "gymClass.trainer",
+        "gymClass.branch"
+    })
+    List<Attendance> findAll();
+
+    @Override
+    @EntityGraph(attributePaths = {
+        "client",
+        "gymClass",
+        "gymClass.trainer",
+        "gymClass.branch"
+    })
+    Page<Attendance> findAll(Pageable pageable);
+
+    @Override
+    @EntityGraph(attributePaths = {
+        "client",
+        "gymClass",
+        "gymClass.trainer",
+        "gymClass.branch"
+    })
+    Optional<Attendance> findById(Integer id);
+
+    @EntityGraph(attributePaths = {
+        "client",
+        "gymClass",
+        "gymClass.trainer",
+        "gymClass.branch"
+    })
     Page<Attendance> findByClient_UserId(Integer userId, Pageable pageable);
 
     boolean existsByClient_UserIdAndGymClass_IdClass(Integer userId, int classId);
 
+    @EntityGraph(attributePaths = {
+        "client",
+        "gymClass",
+        "gymClass.trainer",
+        "gymClass.branch"
+    })
     List<Attendance> findByGymClass_Trainer_UserId(Integer trainerId);
 
+    @EntityGraph(attributePaths = {
+        "client",
+        "gymClass",
+        "gymClass.trainer",
+        "gymClass.branch"
+    })
     Page<Attendance> findByGymClass_Trainer_UserId(Integer trainerId, Pageable pageable);
 
+    @EntityGraph(attributePaths = {
+        "client",
+        "gymClass",
+        "gymClass.trainer",
+        "gymClass.branch"
+    })
     @Query("""
            SELECT a FROM Attendance a
            WHERE a.gymClass IS NOT NULL
@@ -41,8 +85,14 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Integer>
            )
            """)
     List<Attendance> findByTrainerAndBranch(@Param("trainerId") Integer trainerId,
-            @Param("branchId") int branchId);
+                                             @Param("branchId") int branchId);
 
+    @EntityGraph(attributePaths = {
+        "client",
+        "gymClass",
+        "gymClass.trainer",
+        "gymClass.branch"
+    })
     @Query("""
            SELECT a FROM Attendance a
            WHERE a.gymClass IS NOT NULL
@@ -56,11 +106,14 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Integer>
            )
            """)
     Page<Attendance> findByTrainerAndBranch(@Param("trainerId") Integer trainerId,
-            @Param("branchId") int branchId,
-            Pageable pageable);
+                                             @Param("branchId") int branchId,
+                                             Pageable pageable);
 
-    @Query("select distinct a.client from Attendance a "
-            + "where a.gymClass.trainer.userId = :trainerId and a.client is not null")
+    @Query("""
+           SELECT DISTINCT a.client FROM Attendance a
+           WHERE a.gymClass.trainer.userId = :trainerId
+           AND a.client IS NOT NULL
+           """)
     List<User> findDistinctClientsByTrainerId(@Param("trainerId") Integer trainerId);
 
     @Query("""
@@ -77,5 +130,5 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Integer>
            )
            """)
     List<User> findDistinctClientsByTrainerAndBranch(@Param("trainerId") Integer trainerId,
-            @Param("branchId") int branchId);
+                                                      @Param("branchId") int branchId);
 }
