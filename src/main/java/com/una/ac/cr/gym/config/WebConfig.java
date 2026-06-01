5eb3fb12f1c2fc41b04ee55f2f8a43909ff513ad
PC <PC@LAPTOP-PR9AV0C6>
2026-05-31 20:50:52 -0600

    Test delete

--- src/main/java/com/una/ac/cr/gym/config/WebConfig.java
+++ src/main/java/com/una/ac/cr/gym/config/WebConfig.java
@@ -1,8 +1,13 @@
 package com.una.ac.cr.gym.config;
 
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -27,5 +32,22 @@ public class WebConfig implements WebMvcConfigurer {
                         "/webjars/**",
                         "/favicon.ico"
                 );
+          registry.addInterceptor(localeChangeInterceptor());  
     }
+    
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.of("en","us"));
+        return localeResolver;
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+    LocaleChangeInterceptor lci= new LocaleChangeInterceptor();
+    lci.setParamName("lang");
+    return lci;
+            
+    }
+
+
 }
